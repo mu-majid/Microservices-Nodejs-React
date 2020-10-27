ca0b700f7dcc4f1ba59f4d408377ea97fe931c9c
Muhammad Majid <mmajidkamel@gmail.com>
2020-10-27 16:02:56 +0200

    Added ingress ctrl

--- GitTix/auth/src/index.ts
+++ GitTix/auth/src/index.ts
@@ -5,6 +5,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
+
+app.get('/api/users/currentuser', (req: express.Request, res: express.Response) => {
+  res.send('Hi there!');
+})
+
 app.listen(3000, () => {
   console.log()
   console.log('Auth Server Running On 3000!!');
