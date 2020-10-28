7e0176b7404c34ea7a320ec577852598e21b0314
Muhammad Majid <mmajidkamel@gmail.com>
2020-10-28 15:19:38 +0200

    Allow throwing errors inside async handlers in express without next

--- GitTix/auth/src/routes/signup.ts
+++ GitTix/auth/src/routes/signup.ts
@@ -17,7 +17,7 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20 characters')
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
