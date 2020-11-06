f5a551b764103586f6a50b718252229a6efa1164
Muhammad Majid <mmajidkamel@gmail.com>
2020-11-06 16:10:03 +0200

    WIP sign in route handler

--- GitTix/auth/src/routes/signin.ts
+++ GitTix/auth/src/routes/signin.ts
@@ -1,9 +1,37 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('Hi there!');
-});
+router.post('/api/users/signin',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a password.')
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      throw new BadRequestError('Please Sign up.');
+    }
+
+    return res.status(200).send(existingUser);
+
+  });
 
 export { router as signinRouter };
