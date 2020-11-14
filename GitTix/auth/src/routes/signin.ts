1e6fe19509c418035f4c7a58168fecd9f7f7ee01
Muhammad Majid <mmajidkamel@gmail.com>
2020-11-14 14:17:24 +0200

    sign in flow

--- GitTix/auth/src/routes/signin.ts
+++ GitTix/auth/src/routes/signin.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
 import { body } from 'express-validator';
 import { User } from '../models/user';
 import { BadRequestError } from '../errors/bad-request-error';
 import { validateRequest } from '../middlewares/validate-request';
+import { Password } from '../services/password';
 
 const router = express.Router();
 
@@ -23,11 +25,22 @@ router.post('/api/users/signin',
     const existingUser = await User.findOne({ email });
 
     if (!existingUser) {
-      throw new BadRequestError('Please Sign up.');
+      throw new BadRequestError('Invalid Credentials.');
     }
 
-    return res.status(200).send(existingUser);
+    const passwordsMatch = await Password.compare(existingUser.password, password);
 
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid Credentials.');
+    };
+
+    const userJWT = jwt.sign({id: existingUser.id, email: existingUser.email}, process.env.JWT_KEY!);
+    req.session = {
+      jwt: userJWT
+    };
+
+
+    return res.status(200).send(existingUser);
   });
 
 export { router as signinRouter };
