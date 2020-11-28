3bf527fbdcbd406730993e95973936897b8f57f0
Muhammad Majid <mmajidkamel@gmail.com>
2020-11-28 21:19:42 +0200

    refactor

--- GitTix/auth/src/routes/current-user.ts
+++ GitTix/auth/src/routes/current-user.ts
@@ -4,7 +4,7 @@ import { currentUser } from '../middlewares/current-user';
 const router = express.Router();
 
 router.get('/api/users/currentuser', currentUser, (req, res) => {
-  res.send({ currentUser: req.currentUser || null });
+  return res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
