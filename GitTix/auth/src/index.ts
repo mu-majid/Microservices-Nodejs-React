d2fd5badf19054aed755b76ed160f0fde83702a2
Muhammad Majid <mmajidkamel@gmail.com>
2020-11-29 16:42:55 +0200

    refactor index and app files, and edit docker file to not install dev dep

--- GitTix/auth/src/index.ts
+++ GitTix/auth/src/index.ts
@@ -1,35 +1,5 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
 import mongoose from 'mongoose';
-
-// cookie here is for transporting jwt
-const app = express();
-app.set('trust proxy', true); // to trust https traffic from a proxy (ingress)
-app.use(json());
-app.use(cookieSession({
-  signed: false,
-  secure: true // only https connections are allowed
-}))
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from './app';
 
 const start  = async () => {
 
