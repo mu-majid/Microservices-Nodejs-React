8e8d1fec14f23750a466efe0e8fc7378af66ae1a
Muhammad Majid <mmajidkamel@gmail.com>
2020-12-11 22:00:19 +0200

    fix html layout and css class names

--- GitTix/client/components/haeder.js
+++ GitTix/client/components/haeder.js
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 
 export default ({ currentUser }) => {
-
   const links = [
     !currentUser && { label: 'Sign Up', href: '/auth/signup' },
     !currentUser && { label: 'Sign In', href: '/auth/signin' },
@@ -10,25 +9,22 @@ export default ({ currentUser }) => {
     .filter(linkConfig => linkConfig)
     .map(({ label, href }) => {
       return (
-        <li key={href} className="nav-item" >
-          <Link href={href} >
-            <a className="nav-link" >{label}</a>
+        <li key={href} className="nav-item">
+          <Link href={href}>
+            <a className="nav-link">{label}</a>
           </Link>
         </li>
-      )
-    })
+      );
+    });
 
   return (
-    <nav className="nvabar navbar-light bg-light">
-
-      <Link href="/" >
-        <a className="navbar-brand" >GitTix</a>
+    <nav className="navbar navbar-light bg-light">
+      <Link href="/">
+        <a className="navbar-brand">GitTix</a>
       </Link>
 
       <div className="d-flex justify-content-end">
-        <ul className="nav d-flex align-items-center">
-          {links}
-        </ul>
+        <ul className="nav d-flex align-items-center">{links}</ul>
       </div>
     </nav>
   );
