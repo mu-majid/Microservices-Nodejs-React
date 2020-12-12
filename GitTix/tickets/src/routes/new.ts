5e3a7caf3623707765a16ffbd9b0592473a4df75
Muhammad Majid <mmajidkamel@gmail.com>
2020-12-13 01:20:00 +0200

    request validation for new tickets route

--- GitTix/tickets/src/routes/new.ts
+++ GitTix/tickets/src/routes/new.ts
@@ -1,11 +1,19 @@
 import express, { Request, Response } from 'express';
-import { requireAuth } from '@mmkgittix/common';
+import { requireAuth, validateRequest } from '@mmkgittix/common';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
 router.post(
   '/api/tickets',
   requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater than 0'),
+  ],
+  validateRequest,
   async (req: Request, res: Response) => {
 
     return res.sendStatus(200);
