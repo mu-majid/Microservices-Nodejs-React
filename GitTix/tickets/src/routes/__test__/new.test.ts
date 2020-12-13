abf0840927f8b54b831b8f447e7a48d8296f0fae
Muhammad Majid <mmajidkamel@gmail.com>
2020-12-13 20:49:01 +0200

    test cases for new ticket handler

--- GitTix/tickets/src/routes/__test__/new.test.ts
+++ GitTix/tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
   const { status } = await request(app).post('/api/tickets').send({});
@@ -60,4 +61,21 @@ it('returns an error if an invalid price is provided', async () => {
     .expect(400);
 });
 
-it('creates a ticket with valid inputs', async () => {});
+it('creates a ticket with valid inputs', async () => {
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+
+  await request(app)
+  .post('/api/tickets')
+  .set('Cookie', global.signin())
+  .send({
+    title: 'laskdfj',
+    price: 10
+  })
+  .expect(201);
+
+  tickets = await Ticket.find({});
+
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].price).toEqual(10);
+});
--- GitTix/tickets/src/routes/new.ts
+++ GitTix/tickets/src/routes/new.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import { requireAuth, validateRequest } from '@mmkgittix/common';
 import { body } from 'express-validator';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
@@ -15,9 +16,16 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+    const ticket = Ticket.build({
+      price,
+      title,
+      userId: req.currentUser!.id
+    });
+    await ticket.save();
 
-    return res.sendStatus(200);
+    return res.status(201).send(ticket);
   }
 );
 
-export { router as createTicketsRouter }
+export { router as createTicketsRouter };
