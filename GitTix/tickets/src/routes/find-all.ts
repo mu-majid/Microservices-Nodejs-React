ceb11d98c4fbbb4b071b6e079f05f926ffe2210d
Muhammad Majid <mmajidkamel@gmail.com>
2020-12-14 01:06:37 +0200

    todo

--- GitTix/tickets/src/routes/find-all.ts
+++ GitTix/tickets/src/routes/find-all.ts
@@ -3,6 +3,8 @@ import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
+// TODO: Handle limit and skip for pagination
+
 router.get('/api/tickets', async (req: Request, res: Response) => {
   const tickets = await Ticket.find({});
 
