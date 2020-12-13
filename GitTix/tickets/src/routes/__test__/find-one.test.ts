611562d64ac634f4bfed6423e9a7d8937f41ca5e
Muhammad Majid <mmajidkamel@gmail.com>
2020-12-13 20:49:11 +0200

    find one ticket handler

---
+++ GitTix/tickets/src/routes/__test__/find-one.test.ts
@@ -0,0 +1,33 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+
+it('Should return 404 if ticket not found', async () => {
+  await request(app)
+    .get('/api/tickets/notfound')
+    .send()
+    .expect(404)
+});
+
+it('Should return the ticket if found', async () => {
+
+  const title = 'concert';
+  const price = 214;
+
+  // another approach is to insert to DB directly from test case
+  const { body } = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title, price
+    })
+    .expect(201);
+
+  const res = await request(app)
+    .get(`/api/tickets/${body.id}`)
+    .send()
+    .expect(200);
+
+  expect(res.body.title).toEqual(title);
+  expect(res.body.price).toEqual(price);
+});
