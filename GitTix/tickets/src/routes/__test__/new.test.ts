d143317bd5af4568a871bc423e3a6f3803fcc615
Muhammad Majid <mmajidkamel@gmail.com>
2020-12-23 21:56:28 +0200

    redirecting imports with jest mocks

--- GitTix/tickets/src/routes/__test__/new.test.ts
+++ GitTix/tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
+jest.mock('../../nats-wrapper');
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
   const { status } = await request(app).post('/api/tickets').send({});
