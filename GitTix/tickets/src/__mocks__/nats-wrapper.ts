838f582a1513078dc332446f6a04ec20ce97b58c
Muhammad Majid <mmajidkamel@gmail.com>
2020-12-23 23:36:27 +0200

    jest mock implementation for mocked function

--- GitTix/tickets/src/__mocks__/nats-wrapper.ts
+++ GitTix/tickets/src/__mocks__/nats-wrapper.ts
@@ -1,7 +1,7 @@
 export const natsWrapper = {
   client: {
-    publish: (subject: string, data: string, callback: () => void) => {
+    publish: jest.fn().mockImplementation((subject: string, data: string, callback: () => void) => {
       callback();
-    }
+    })
   }
 }
