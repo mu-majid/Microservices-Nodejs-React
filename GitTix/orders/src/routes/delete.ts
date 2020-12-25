a4f33a5d187079b1b30eceb4a4dc449dc4ea3513
Muhammad Majid <mmajidkamel@gmail.com>
2020-12-26 00:19:23 +0200

    sift delete order route handler

--- GitTix/orders/src/routes/delete.ts
+++ GitTix/orders/src/routes/delete.ts
@@ -1,12 +1,30 @@
 import express, { Request, Response } from 'express';
+import { OrderStatus, NotFoundError, NotAuthorizedError, requireAuth } from '@mmkgittix/common';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
 router.delete(
   '/api/orders/:orderId',
+  requireAuth,
   async (req: Request, res: Response) => {
 
-    return res.send({});
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    order.status = OrderStatus.CANCELLED;
+    await order.save();
+
+    // publishing an event saying this was cancelled!
+
+    res.status(204).send(order);
   }
 );
 
