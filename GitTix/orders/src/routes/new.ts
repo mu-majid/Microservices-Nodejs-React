a317552c45f1c46aa8dab5ab83f83441fc51f23e
Muhammad Majid <mmajidkamel@gmail.com>
2020-12-25 19:28:15 +0200

    wip create new order route handler

--- GitTix/orders/src/routes/new.ts
+++ GitTix/orders/src/routes/new.ts
@@ -1,9 +1,22 @@
 import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@mmkgittix/common';
+import { body } from 'express-validator';
+import mongoose from 'mongoose';
 
 const router = express.Router();
 
+// we should not be validating it is mongo id, because it introduce coupling between services.(Assume tickets service is removed entirely and event are coming from other place)
 router.post(
   '/api/orders',
+  requireAuth,
+  [
+    body('ticketId') 
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('a valid ticketId Must be provided')
+  ],
+  validateRequest,
   async (req: Request, res: Response) => {
 
     return res.send({});
