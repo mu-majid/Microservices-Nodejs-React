1ce4144b4e6f7ff4bd1bb7bf16e60209989c8f8a
Muhammad Majid <mmajidkamel@gmail.com>
2020-12-26 00:20:50 +0200

    delete route test cases

---
+++ GitTix/orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,36 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+import { Order } from '../../models/order';
+import { OrderStatus } from '@mmkgittix/common';
+
+it('marks an order as cancelled', async () => {
+  // create a ticket with Ticket Model
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+  // make a request to create an order
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // make a request to cancel the order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(204);
+
+  // expectation to make sure the thing is cancelled
+  const updatedOrder = await Order.findById(order.id);
+
+  expect(updatedOrder!.status).toEqual(OrderStatus.CANCELLED);
+});
+
+it.todo('emits a order cancelled event');
