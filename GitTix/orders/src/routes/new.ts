a1dced01808b5d0d4ba690ec7f6489736c501287
Muhammad Majid <mmajidkamel@gmail.com>
2020-12-25 22:59:40 +0200

    Wip new order route handler

--- GitTix/orders/src/routes/new.ts
+++ GitTix/orders/src/routes/new.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@mmkgittix/common';
+import { requireAuth, validateRequest, NotFoundError, OrderStatus, BadRequestError } from '@mmkgittix/common';
 import { body } from 'express-validator';
 import mongoose from 'mongoose';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
@@ -18,6 +20,22 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const  { ticketId } = req.body;
+
+    // Find Ticket to reserve.
+    const foundTicket = await Ticket.findById(ticketId);
+    if (!foundTicket) {
+      throw new NotFoundError();
+    }
+
+    // Make sure ticket is not reserved
+    const isReserved = Order.findOne(
+      { ticket: foundTicket, status: { $in: [OrderStatus.CREATED, OrderStatus.COMPLETE, OrderStatus.AWAITING_PAYMENT] } }
+    );
+
+    if (isReserved) {
+      throw new BadRequestError('Ticket is already reserved.');
+    }
 
     return res.send({});
   }
