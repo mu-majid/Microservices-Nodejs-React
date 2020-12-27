ac2999e8f13009a5ef7a54573ae31b1f1bead30f
Muhammad Majid <mmajidkamel@gmail.com>
2020-12-27 15:44:08 +0200

    test case for publishing new order event

--- GitTix/orders/src/routes/__test__/new.test.ts
+++ GitTix/orders/src/routes/__test__/new.test.ts
@@ -4,6 +4,7 @@ import { app } from '../../app';
 import { Order } from '../../models/order';
 import { Ticket } from '../../models/ticket';
 import { OrderStatus } from '@mmkgittix/common';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('returns an error if the ticket does not exist', async () => {
   const ticketId = mongoose.Types.ObjectId();
@@ -48,4 +49,20 @@ it('reserves a ticket', async () => {
     .set('Cookie', global.signin())
     .send({ ticketId: ticket.id })
     .expect(201);
+});
+
+it('emits an order created event', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
