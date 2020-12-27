04a44e19d9a4c8b76b8f89e1c436150be5c32b99
Muhammad Majid <mmajidkamel@gmail.com>
2020-12-27 15:44:50 +0200

    test case for order deleted event

--- GitTix/orders/src/routes/__test__/delete.test.ts
+++ GitTix/orders/src/routes/__test__/delete.test.ts
@@ -3,6 +3,7 @@ import { app } from '../../app';
 import { Ticket } from '../../models/ticket';
 import { Order } from '../../models/order';
 import { OrderStatus } from '@mmkgittix/common';
+import { natsWrapper } from '../../nats-wrapper';
 
 it('marks an order as cancelled', async () => {
   // create a ticket with Ticket Model
@@ -33,4 +34,27 @@ it('marks an order as cancelled', async () => {
   expect(updatedOrder!.status).toEqual(OrderStatus.CANCELLED);
 });
 
-it.todo('emits a order cancelled event');
+it('emits a order cancelled event', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+  // make a request to create an order
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // make a request to cancel the order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(204);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
