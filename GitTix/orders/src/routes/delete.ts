b516cd2cb086db1dce70d0b3e00c7a2e1d7bd443
Muhammad Majid <mmajidkamel@gmail.com>
2020-12-27 15:42:06 +0200

    publish cancel order event

--- GitTix/orders/src/routes/delete.ts
+++ GitTix/orders/src/routes/delete.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from 'express';
 import { OrderStatus, NotFoundError, NotAuthorizedError, requireAuth } from '@mmkgittix/common';
 import { Order } from '../models/order';
+import { OrderCancelledPublisher } from '../events/publishers/order-cancelled-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -11,7 +13,7 @@ router.delete(
 
     const { orderId } = req.params;
 
-    const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate('ticket');
 
     if (!order) {
       throw new NotFoundError();
@@ -23,6 +25,12 @@ router.delete(
     await order.save();
 
     // publishing an event saying this was cancelled!
+    new OrderCancelledPublisher(natsWrapper.client).publish({
+      id: order.id,
+      ticket: {
+        id: order.ticket.id
+      }
+    })
 
     res.status(204).send(order);
   }
