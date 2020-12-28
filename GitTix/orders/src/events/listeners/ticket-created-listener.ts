d34c2e57a504e5fa36548b1bdf2dd8b341751b34
Muhammad Majid <mmajidkamel@gmail.com>
2020-12-28 17:45:24 +0200

    WIP orders service listener for ticket created

---
+++ GitTix/orders/src/events/listeners/ticket-created-listener.ts
@@ -0,0 +1,23 @@
+import { Message } from 'node-nats-streaming';
+import { Subjects, Listener, TicketCreatedEvent } from '@mmkgittix/common';
+import { Ticket } from '../../models/ticket';
+import { queueGroupName } from './queue-group-name';
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TICKET_CREATED = Subjects.TICKET_CREATED;
+
+  // Reminder: This group is used by nats to send the event to only one member(listener/service/subscriber) of the group
+  queueGroupName: string = queueGroupName;
+
+  async onMessage(data: TicketCreatedEvent['data'], msg: Message): void {
+    const { title, price } = data;
+
+    const ticket = Ticket.build({
+      title, price
+    });
+    await ticket.save();
+
+    msg.ack();
+  }
+
+}
