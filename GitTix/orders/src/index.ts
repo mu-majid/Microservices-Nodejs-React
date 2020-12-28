e96174fc526d88b1003177fdaf5e0716fb271a5f
Muhammad Majid <mmajidkamel@gmail.com>
2020-12-28 18:01:24 +0200

    init listeners

--- GitTix/orders/src/index.ts
+++ GitTix/orders/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
+import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
+import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
 
 const start  = async () => {
 
@@ -32,6 +34,10 @@ const start  = async () => {
     });
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
+
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
+
 
     await mongoose.connect(process.env.MONGO_URI, {
       useCreateIndex: true,
