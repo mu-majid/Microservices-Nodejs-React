ff354454b5c331ace25fc58a73f1ac28abe75df5
Muhammad Majid <mmajidkamel@gmail.com>
2020-12-31 17:15:57 +0200

    Testing concurrency issues script

---
+++ GitTix/concurrency-test-script/index.js
@@ -0,0 +1,52 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+const axios = require('axios');
+
+/**
+ * Important Note:
+ * =================
+ * 
+ * For this test to break the current implementation of the services (No Concrruncy Handling)
+ * I had to replicate orders service and tickets service to 4 instances each.
+ * 
+ * After running this script, you'll notice that all tickets in tickets-srv have price of 15 which correct
+ * But some tickets in orders service will have prices of 10 which out of sync and this is a violation to our data integrity
+ * 
+ * This was solved by applying optimistic concurrency control.
+ */
+
+const cookie =
+  'express:sess=eyJqd3QiOiJleUpoYkdjaU9pSklVekkxTmlJc0luUjVjQ0k2SWtwWFZDSjkuZXlKcFpDSTZJalZsT0dZME5qQTFOak5oWVRkbE1EQXlZMkl3TUdSbU5pSXNJbVZ0WVdsc0lqb2lkR1Z6ZERFd1FIUmxjM1F1WTI5dElpd2lhV0YwSWpveE5UZzJOVE0yTlRBMWZRLlh3R3p5UVZHYnFSaHZ1YWJDMTdsYzNtYlpQNU1XMnl1UU1kODU1Y0hEM3MifQ==';
+
+const doRequest = async () => {
+  const { data } = await axios.post(
+    `https://ticketing.dev/api/tickets`,
+    { title: 'ticket', price: 5 },
+    {
+      headers: { cookie },
+    }
+  );
+
+  await axios.put(
+    `https://ticketing.dev/api/tickets/${data.id}`,
+    { title: 'ticket', price: 10 },
+    {
+      headers: { cookie },
+    }
+  );
+
+  axios.put(
+    `https://ticketing.dev/api/tickets/${data.id}`,
+    { title: 'ticket', price: 15 },
+    {
+      headers: { cookie },
+    }
+  );
+
+  console.log('Request complete');
+};
+
+(async () => {
+  for (let i = 0; i < 200; i++) {
+    doRequest();
+  }
+})();
