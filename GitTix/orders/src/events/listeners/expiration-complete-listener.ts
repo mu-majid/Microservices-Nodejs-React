9c3ecd423623428d4c8c11a2e963f24adafb70f5
Muhammad Majid <mmajidkamel@gmail.com>
2021-01-03 15:27:02 +0200

    return early if expiration was targetting a complete event

--- GitTix/orders/src/events/listeners/expiration-complete-listener.ts
+++ GitTix/orders/src/events/listeners/expiration-complete-listener.ts
@@ -22,6 +22,10 @@ export class ExpirationCompleteListener extends Listener<
       throw new Error('Order not found');
     }
 
+    if (order.status === OrderStatus.COMPLETE) {
+      return msg.ack();
+    }
+
     order.set({
       status: OrderStatus.CANCELLED,
     });
