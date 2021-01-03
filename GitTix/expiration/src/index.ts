6cd83752b368e86542682cc8b966745fcdbe3ae0
Muhammad Majid <mmajidkamel@gmail.com>
2021-01-03 13:36:43 +0200

    installing a couple of dependencies, and fix index file errors

--- GitTix/expiration/src/index.ts
+++ GitTix/expiration/src/index.ts
@@ -1,18 +1,6 @@
-import mongoose from 'mongoose';
-import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
-import { OrderCreatedListener } from './events/listeners/order-created-listener';
-import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start  = async () => {
-
-  if (!process.env.JWT_KEY) {
-    throw new Error('JWT_KEY env should be defined');
-  }
-
-  if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI env should be defined');
-  }
 
   if (!process.env.NATS_URL) {
     throw new Error('NATS_URL env should be defined');
@@ -35,25 +23,11 @@ const start  = async () => {
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
 
-    new OrderCreatedListener(natsWrapper.client).listen();
-    new OrderCancelledListener(natsWrapper.client).listen();
-
-    await mongoose.connect(process.env.MONGO_URI, {
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-      useNewUrlParser: true
-    });
-
-    console.log('Connected to MongoDB');
 
   } 
   catch (error) {
     console.error(error);
   }
-
-  app.listen(3000, () => {
-    console.log('Tickets Server on port 3000!!!');
-  });
 }
 
 start();
