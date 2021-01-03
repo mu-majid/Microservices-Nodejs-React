b1ea95168ad3676a4e3b7703738e22deceea76a2
Muhammad Majid <mmajidkamel@gmail.com>
2021-01-03 15:06:41 +0200

    listen to expiration complete event in orders service

---
+++ GitTix/orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,39 @@
+import {
+  Listener,
+  Subjects,
+  ExpirationCompleteEvent,
+  OrderStatus,
+} from '@mmkgittix/common';
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+import { Order } from '../../models/order';
+import { OrderCancelledPublisher } from '../publishers/order-cancelled-publisher';
+
+export class ExpirationCompleteListener extends Listener<
+  ExpirationCompleteEvent
+> {
+  queueGroupName = queueGroupName;
+  subject: Subjects.EXPIRATION_COMPLETE = Subjects.EXPIRATION_COMPLETE;
+
+  async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+    const order = await Order.findById(data.orderId).populate('ticket');
+
+    if (!order) {
+      throw new Error('Order not found');
+    }
+
+    order.set({
+      status: OrderStatus.CANCELLED,
+    });
+    await order.save();
+    await new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+
+    msg.ack();
+  }
+}
