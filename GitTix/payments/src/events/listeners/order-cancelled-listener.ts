fa1324f1980524b1742714c0941a64be00372257
Muhammad Majid <mmajidkamel@gmail.com>
2021-01-04 18:25:35 +0200

    order canceled listener in pay-srv

---
+++ GitTix/payments/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,23 @@
+import { Listener, OrderCancelledEvent, Subjects, OrderStatus } from "@mmkgittix/common";
+import { queueGroupName } from "./group-name";
+import { Message } from 'node-nats-streaming';
+import { Order } from "../../models/order";
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  subject: Subjects.ORDER_CANCELLED = Subjects.ORDER_CANCELLED;
+  queueGroupName: string = queueGroupName;
+  async onMessage(data: OrderCancelledEvent['data'], msg: Message): Promise<void> {
+    
+    const orderToCancel = await Order.findById(data.id);
+
+    if (!orderToCancel) {
+      throw new Error('Order Not Found');
+    }
+
+    orderToCancel.set({ status: OrderStatus.CANCELLED });
+    await orderToCancel.save();
+
+    msg.ack();
+  }
+  
+}
