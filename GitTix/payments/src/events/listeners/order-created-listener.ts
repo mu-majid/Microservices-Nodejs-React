9163972493673612a1d14f1277b49296b25c448c
Muhammad Majid <mmajidkamel@gmail.com>
2021-01-04 18:17:19 +0200

    init impl for order:created event in pay-srv

--- GitTix/payments/src/events/listeners/order-created-listener.ts
+++ GitTix/payments/src/events/listeners/order-created-listener.ts
@@ -1,12 +1,23 @@
 import { Listener, OrderCreatedEvent, Subjects } from "@mmkgittix/common";
 import { queueGroupName } from "./group-name";
 import { Message } from 'node-nats-streaming';
+import { Order } from "../../models/order";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.ORDER_CREATED = Subjects.ORDER_CREATED;
   queueGroupName: string = queueGroupName;
-  onMessage(data: OrderCreatedEvent['data'], msg: Message): void {
-    throw new Error("Method not implemented.");
-  }
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message): Promise<void> {
+    
+    const order = Order.build({
+      id: data.id,
+      price: data.ticket.price,
+      status: data.status,
+      userId: data.userId,
+      version: data.version
+    });
 
+    await order.save();
+
+    msg.ack();
+  }
 }
