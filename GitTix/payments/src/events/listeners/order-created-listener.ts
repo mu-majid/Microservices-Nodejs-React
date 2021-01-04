567949cf3bb39f6925e4666d820e841b2cb6268f
Muhammad Majid <mmajidkamel@gmail.com>
2021-01-04 18:12:46 +0200

    listen for order created event in payments service

---
+++ GitTix/payments/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,12 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@mmkgittix/common";
+import { queueGroupName } from "./group-name";
+import { Message } from 'node-nats-streaming';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.ORDER_CREATED = Subjects.ORDER_CREATED;
+  queueGroupName: string = queueGroupName;
+  onMessage(data: OrderCreatedEvent['data'], msg: Message): void {
+    throw new Error("Method not implemented.");
+  }
+
+}
