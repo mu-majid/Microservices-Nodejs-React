0e0023784dcb9712125129e70bed8876960efd00
Muhammad Majid <mmajidkamel@gmail.com>
2021-01-04 19:19:10 +0200

    WIP new charge route implementation

--- GitTix/payments/src/routes/new.ts
+++ GitTix/payments/src/routes/new.ts
@@ -16,6 +16,20 @@ router.post(
   [body('token').not().isEmpty(), body('orderId').not().isEmpty()],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError('Cannot pay for an cancelled order');
+    }
+
     res.send({ success: true });
   }
 );
