31c8ea8a6d0921b0fd8c1930279c12474ef6e6aa
Muhammad Majid <mmajidkamel@gmail.com>
2021-01-04 18:07:13 +0200

    replicating orders inside payments service

---
+++ GitTix/payments/src/models/interfaces.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+import { OrderStatus } from '@mmkgittix/common';
+ 
+// describe creating new order
+export interface INewOrder {
+  id: string;
+  version: number;
+  userId: string;
+  price: number;
+  status: OrderStatus;
+}
+
+// describes properties on order document (getorder)
+export interface IOrderDoc extends mongoose.Document {
+  version: number;
+  userId: string;
+  price: number;
+  status: OrderStatus;
+  // createdAt: string;
+  // updatedAt: string;
+}
+
+// describes properties on Order model to make typescript able to validate our doc to be created
+export interface IOrderModelProps extends mongoose.Model<IOrderDoc> {
+  build(order: INewOrder): IOrderDoc;
+}
---
+++ GitTix/payments/src/models/order.ts
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+import { INewOrder, IOrderDoc, IOrderModelProps } from './interfaces';
+import { OrderStatus } from '@mmkgittix/common';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+
+const orderSchema = new mongoose.Schema({
+  status: {
+    type: String,
+    required: true,
+    enum: Object.values(OrderStatus)
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  userId: {
+    type: String,
+    required: true
+  }
+}, {
+  toJSON: {
+    versionKey: false,
+    transform(doc, ret) {
+      ret.id = ret._id;
+      delete ret._id;
+    }
+  }
+});
+
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
+
+orderSchema.statics.build = (order: INewOrder) => {
+  // we have to spread attrs and not just call new Order(order), to ensure _id property is the same as order.id from orders service
+  return new Order({
+    _id: order.id,
+    version: order.version,
+    status: order.status,
+    price: order.price,
+    userId: order.userId
+  });
+};
+
+const Order = mongoose.model<IOrderDoc, IOrderModelProps>('Order', orderSchema);
+
+export { Order };
