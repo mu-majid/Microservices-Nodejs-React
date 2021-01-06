62a52e24ffe87cff64a5fb6e98474e183eee03d4
Muhammad Majid <mmajidkamel@gmail.com>
2021-01-06 16:48:43 +0200

    TEST: mocking stripe client

--- GitTix/payments/src/routes/__test__/new.test.ts
+++ GitTix/payments/src/routes/__test__/new.test.ts
@@ -3,6 +3,9 @@ import request from 'supertest';
 import { OrderStatus } from '@mmkgittix/common';
 import { app } from '../../app';
 import { Order } from '../../models/order';
+import { stripe } from '../../stripe';
+
+jest.mock('../../stripe');
 
 it('returns a 404 when purchasing an order that does not exist', async () => {
   await request(app)
@@ -54,4 +57,30 @@ it('returns a 400 when purchasing a cancelled order', async () => {
       token: 'asdlkfj',
     })
     .expect(400);
+});
+
+it('returns a 204 with valid inputs', async () => {
+  const userId = mongoose.Types.ObjectId().toHexString();
+  const order = Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    userId,
+    version: 0,
+    price: 20,
+    status: OrderStatus.CREATED,
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signin(userId))
+    .send({
+      token: 'tok_visa',
+      orderId: order.id,
+    })
+    .expect(201);
+
+  const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+  expect(chargeOptions.source).toEqual('tok_visa');
+  expect(chargeOptions.amount).toEqual(20 * 100);
+  expect(chargeOptions.currency).toEqual('usd');
 });
