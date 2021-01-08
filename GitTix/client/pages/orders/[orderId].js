df61b9c0cd60c7153feefddef7ba4cdacdaa1732
Muhammad Majid <mmajidkamel@gmail.com>
2021-01-08 16:00:27 +0200

    showing expiration time on a specifiic order

--- GitTix/client/pages/orders/[orderId].js
+++ GitTix/client/pages/orders/[orderId].js
@@ -1,7 +1,37 @@
-const OrderShow = () => {
-  return (
-    <div>OrderShow</div>
-  );
+import { useEffect, useState } from 'react';
+
+const OrderShow = ({ order }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [order]); 
+  // dependency array should be empty, but i placed order here to make a warning go away, 
+  // and also order is not changiung at all,
+  // so this function will only run once when the component is first rendered
+
+  if (timeLeft < 0) {
+    return <div>Order Expired</div>;
+  }
+
+  return <div>Time left to pay: {timeLeft} seconds</div>;
+};
+
+OrderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
+
+  return { order: data };
 };
 
 export default OrderShow;
