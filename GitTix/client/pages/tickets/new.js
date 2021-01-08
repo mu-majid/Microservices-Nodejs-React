18b29e9582c779a44f4d10f70cc4acaec5b3cf2b
Muhammad Majid <mmajidkamel@gmail.com>
2021-01-08 14:24:27 +0200

    state for new ticket component

--- GitTix/client/pages/tickets/new.js
+++ GitTix/client/pages/tickets/new.js
@@ -1,4 +1,19 @@
+import { useState } from 'react';
+
 const NewTicket = () => {
+  const [title, setTitle] = useState('');
+  const [price, setPrice] = useState('');
+
+  const onBlur = () => {
+    const value = parseFloat(price);
+
+    if (isNaN(value)) {
+      return;
+    }
+
+    setPrice(value.toFixed(2));
+  };
+
   return (
     <div>
       <h1>Create a Ticket</h1>
@@ -6,21 +21,24 @@ const NewTicket = () => {
         <div className="form-group">
           <label>Title</label>
           <input
-
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
             className="form-control"
           />
         </div>
         <div className="form-group">
           <label>Price</label>
           <input
-
+            value={price}
+            onBlur={onBlur}
+            onChange={(e) => setPrice(e.target.value)}
             className="form-control"
           />
         </div>
         <button className="btn btn-primary">Submit</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default NewTicket;
