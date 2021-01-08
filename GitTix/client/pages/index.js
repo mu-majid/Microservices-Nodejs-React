eed31047532e1a05713eccce0d22f0c038067a89
Muhammad Majid <mmajidkamel@gmail.com>
2021-01-08 14:54:58 +0200

    WIP showing ticket details component

--- GitTix/client/pages/index.js
+++ GitTix/client/pages/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 
 const LandingPage = ({ currentUser, tickets }) => {
   const ticketList = tickets.map(ticket => {
@@ -5,6 +6,12 @@ const LandingPage = ({ currentUser, tickets }) => {
       <tr key={ticket.id}>
         <td>{ticket.title}</td>
         <td>{ticket.price}</td>
+        <td>
+          {/* href here is the file path, and as is the actual url */}
+          <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+            <a className="nav-link">View</a>
+          </Link>
+        </td>
       </tr>
     );
   });
@@ -20,6 +27,7 @@ const LandingPage = ({ currentUser, tickets }) => {
           <tr>
             <th>Title</th>
             <th>Price</th>
+            <th>Link</th>
           </tr>
         </thead>
         <tbody>
---
+++ GitTix/client/pages/tickets/[ticketId].js
@@ -0,0 +1,9 @@
+const TicketShow = () => {
+  return (
+    <div>
+      TicketShow
+    </div>
+  );
+}
+
+export default TicketShow;
