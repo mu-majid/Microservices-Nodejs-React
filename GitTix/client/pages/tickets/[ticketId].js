159665220ca3f1f6e3b9dab344569363ea8d11e7
Muhammad Majid <mmajidkamel@gmail.com>
2021-01-08 15:14:29 +0200

    showing details about ticket and allow ordering it

--- GitTix/client/pages/tickets/[ticketId].js
+++ GitTix/client/pages/tickets/[ticketId].js
@@ -1,9 +1,30 @@
-const TicketShow = () => {
+import useRequest from "../../hooks/use-request";
+
+const TicketShow = ({ ticket }) => {
+  const { doRequest, errors } = useRequest({
+    url: '/api/orders',
+    method: 'post',
+    body: {
+      ticketId: ticket.id
+    },
+    onSuccess: (order) => console.log(order)
+  });
+
   return (
     <div>
-      TicketShow
+      <h1>{ticket.title}</h1>
+      <h4>Price: {ticket.price}</h4>
+      {errors}
+      <button onClick={doRequest} className="btn btn-primary">Purchase</button>
     </div>
   );
+};
+
+TicketShow.getInitialProps = async (context, client) => {
+  const { ticketId } = context.query;
+  const { data } = await client.getInitialProps(`/api/tickets/${ticketId}`);
+
+  return { ticket: data };
 }
 
 export default TicketShow;
