4cfcb736e46fa52f32e0543bf847cf4dfed2e296
Muhammad Majid <mmajidkamel@gmail.com>
2021-01-09 03:45:17 +0200

    fix arrow function export in next app

--- GitTix/client/hooks/use-request.js
+++ GitTix/client/hooks/use-request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useState } from 'react';
 
-export default ({ url, method, body, onSuccess }) => {
+const useRequest = ({ url, method, body, onSuccess }) => {
   const [errors, setErrors] = useState(null);
 
   const doRequest = async (props = {}) => {
@@ -31,3 +31,5 @@ export default ({ url, method, body, onSuccess }) => {
 
   return { doRequest, errors };
 };
+
+export default useRequest;
